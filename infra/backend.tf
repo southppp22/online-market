c05cec700acd8b6325a8f5e15be0bf18f14da696
namin <tnddl032@gmail.com>
2026-08-04 19:50:43 +0900

    Terraform 로컬 전용 전환: infra/ gitignore, infra.yml 삭제

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stonlinemarkettfstate"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-    use_azuread_auth     = true
-  }
-}
--- infra/registry.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "main" {
-  name                = "cronlinemarketprod"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku                 = "Basic"
-  admin_enabled       = false
-}
